0a52d49313a3e3a6765de66d977cf75b1af98249
nithyaganeshm-dev <mnithyaganesh@gmail.com>
2024-11-12 19:57:08 +0530

    files modified

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,10 +9,11 @@ const Home = () => {
         <div className="row">
           <div className="col-12 col-lg-6">
             <div className='home-content'>
-              <h1 className='home-title'>Hey, I'm Nithyaganesh, An Aspiring Full Stack Developer</h1>
-              <p className='home-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quos placeat alias aperiam neque, adipisci dolore. Quos quod ipsa autem?</p>
+              <h1 className='home-title'>Hello Everyone, I'm Nithyaganesh</h1>
+              <p className='home-description'>I’m from Salem, and I’m currently working on my Bachelor’s in Computer Science and Engineering at the Knowledge Institute of Technology. Tech has always been my passion, and I’ve been especially drawn to Full Stack Development – the MERN stack, in particular. It’s amazing to see how coding can transform ideas into real, impactful solutions.
+                I’m always sharpening my skills, and NxtWave Academy has been a huge part of that journey.</p>
               <Button homeBtn="home-btn-one">Book a Call</Button>
-              <Button homeBtn="home-btn-two">Download Resume</Button>
+              <Button homeBtn="home-btn-two">Download CV</Button>
             </div>
           </div>
           <div className="col-12 col-md-6 d-none d-lg-block">
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg" data-bs-theme="light">
+        <nav className="navbar navbar-expand-lg sticky-top" data-bs-theme="light">
             <div className="container">
-                <a className="navbar-brand" href="#">NITHYAGANESH M</a>
+                <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
